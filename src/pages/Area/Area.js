f354400bb7f5ed5eb85c7bbe4996e9f8add836fd
Diane MPACKO <diane@MacBook-Air-de-Diane.local>
2022-04-28 23:44:44 +0200

    add /area searchbar

--- src/pages/Area/Area.js
+++ src/pages/Area/Area.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 import CommentsCol from '../../components/Layout/CommentsCol/CommentsCol';
+import SearchBar from '../../components/SearchBar/SearchBar';
 import './Area.css';
 
 import { getRessources } from '../../services/soccerapi_services';
 
 const Area = () => {
   const [areas, setAreas] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+}
 
   useEffect(() => {
     async function getDatas(){
@@ -21,10 +27,28 @@ const Area = () => {
         <CommentsCol />
         <Col xs={8} >
           <div className='layout-cols'>
-            <h1>Today's games</h1>
+            <h1>Countries and areas</h1>
             <Container>
               <Row>
-                {areas.map(area => (
+                <Col xs={6}>
+                  <SearchBar
+                  className="search__input"
+                  type="text"
+                  placeholder="Search a country"
+                  name="searchbar"
+                  value={searchValue}
+                  onChange={handleSearch}
+                  />
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <Row>
+                {areas
+                .filter((area) => {
+                  return area.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0;
+              })
+                .map(area => (
                   <Col xs={4} >
                     <Card key={area.id} className="text-center" style={{ marginTop: "1rem" }} >
                       <ListGroup variant="flush">
