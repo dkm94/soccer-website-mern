7e45675df56a975f5e63fc4fb7a904417ff673f7
Diane MPACKO <diane@MacBook-Air-de-Diane.local>
2022-04-28 10:49:08 +0200

    add /games data

--- src/pages/Match/Match.js
+++ src/pages/Match/Match.js
@@ -1,15 +1,128 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import CommentsCol from '../../components/Layout/CommentsCol/CommentsCol';
-import MainCol from '../../components/Layout/MainCol/MainCol';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './Match.css';
 
+import { getMatches } from '../../services/soccerapi_services';
+
 const Match = () => {
+  const [games, setGames] = useState([]);
+  const [selectedDateFrom, setSelectedDateFrom] = useState(null);
+  const [selectedDateTo, setSelectedDateTo] = useState(null);
+
+  const searchMatches = (from, to) => {
+      async function getDatas(){
+        await getMatches("matches", from, to).then(res => setGames(res));
+      }
+      return getDatas();
+    }
+
+  // Date format
+  const dateFrom = selectedDateFrom?.toLocaleDateString("en-CA");
+  const dateTo = selectedDateTo?.toLocaleDateString("en-CA");
+
+  console.log(games);
+
   return (
     <Container className='layout' >
       <Row>
         <CommentsCol />
-        <MainCol/>
+        <Col lg={8} >
+          <div className='layout-cols'>
+            <h1>Search</h1>
+            <Container>
+              <Row>
+                <Col>
+                  <DatePicker
+                    selected={selectedDateFrom}
+                    onChange={date => setSelectedDateFrom(date)}
+                    placeholderText={'From'}
+                    filterDate={date => date.getDay() !== 6 && date.getDay() !== 0} // weekends cancel
+                    showYearDropdown // year show and scrolldown alos
+                    scrollableYearDropdown
+                  />
+                </Col>
+                <Col>
+                <DatePicker
+                  selected={selectedDateTo}
+                  onChange={date => setSelectedDateTo(date)}
+                  placeholderText={'To'}
+                  filterDate={date => date.getDay() !== 6 && date.getDay() !== 0} // weekends cancel
+                  showYearDropdown // year show and scrolldown alos
+                  scrollableYearDropdown
+                />
+                </Col>
+                <Col>
+                  <Button
+                    onClick={() => searchMatches(dateFrom, dateTo)}
+                  >
+                    Search
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
+            {games.map(match => (
+              <Card className="text-center" style={{ marginTop: "1rem" }} >
+              <Card.Header>{match.competition.name} / {match.competition.area.name}</Card.Header>
+              <Card.Body>
+                <Container>
+                  <Row>
+                    <Col>
+                      <Card.Text>
+                        {match.group}
+                      </Card.Text>
+                  </Col>
+                  </Row>
+                  <Row className="justify-content-md-center">
+                    <Col xs={4} >
+                      <Card.Title>{match.score.fullTime.homeTeam}</Card.Title>
+                      <Card.Title>{match.homeTeam.name}</Card.Title>
+                    </Col>
+                    <Col xs={2}>
+                      <Card.Title><span>-</span></Card.Title>
+                  
+                    </Col>
+                    <Col xs={4}>
+                      <Card.Title>{match.score.fullTime.awayTeam}</Card.Title>
+                      <Card.Title>{match.awayTeam.name}</Card.Title>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Card.Text>
+                        Half-time score: {match.score.halfTime.homeTeam} - {match.score.halfTime.awayTeam}
+                      </Card.Text>
+                  </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Card.Text>
+                      Full-time score: {match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}
+                      </Card.Text>
+                  </Col>
+                  </Row><Row>
+                    <Col>
+                      <Card.Text>
+                        {!match.score.extraTime.homeTeam && !match.score.extraTime.awayTeam ? null : `Extra-time score: ${match.score.extraTime.homeTeam} - ${match.score.extraTime.awayTeam}` }
+                      </Card.Text>
+                  </Col>
+                  </Row><Row>
+                    <Col>
+                      <Card.Text>
+                        {match.group}
+                      </Card.Text>
+                  </Col>
+                  </Row>
+                </Container>
+                
+              </Card.Body>
+              <Card.Footer className="text-muted">{match.status}</Card.Footer>
+            </Card>
+            ))}
+          </div>
+        </Col>
       </Row>
     </Container>
   )
--- src/services/soccerapi_services.js
+++ src/services/soccerapi_services.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://api.football-data.org/v2';
 const headersId = "9a56582ee8c04485a073a7ae417482f8";
 
-export {BASE_URL, getRessources, getRessource};
+export {BASE_URL, getRessources, getRessource, getMatches};
 
 function getRessources(name) {
   const url = `${BASE_URL}/${name}`;
@@ -12,6 +12,19 @@ function getRessources(name) {
 function getRessource(name, id) {
   const url = `${BASE_URL}/${name}/${id}`;
   return axios.get(url, { headers: { "X-Auth-Token": `${headersId}` }}).then((response) => response.data);
+}
+
+function getMatches(name, from, to) {
+  const url = `${BASE_URL}/${name}`;
+  return axios
+        .get(url, 
+          { 
+            headers: { "X-Auth-Token": `${headersId}` }, 
+            params: {
+              dateFrom : from, 
+              dateTo : to
+            }
+          }).then((response) => response.data[`${name}`]);
 }
 
 
