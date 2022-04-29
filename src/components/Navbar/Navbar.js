d2199a2a0e6c683a40ba0a2569cb808f4397d62e
Diane MPACKO <diane@MacBook-Air-de-Diane.local>
2022-04-29 13:35:42 +0200

    collapse nav config

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,19 +14,22 @@ const Navigation = ({ token }) => {
 
   return (
     <>
-    <Navbar className='nav-style' style={{ position: 'fixed', zIndex: 1000 }} bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" className='nav-style' style={{ position: 'fixed', zIndex: 1000 }} bg="light" variant="light">
       <Container>
-      <Navbar.Brand href="#home">2LEFOOT</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/players">Players</Nav.Link>
-        <Nav.Link href="/area">Areas</Nav.Link>
-        <Nav.Link href="/competition">Competitions</Nav.Link>
-        <Nav.Link href="/teams">Teams</Nav.Link>
-        <Nav.Link href="/games">Matches</Nav.Link>
-        <Nav.Link href="/myfavorites">My Favorites</Nav.Link>
-        {!token ? <Nav.Link href="/auth" className='login-button' >Log in/Register</Nav.Link> : <Nav.Link onClick={logOut} href="/" className='login-button' >Log out</Nav.Link>}
-      </Nav>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Brand href="/">2LEFOOT</Navbar.Brand>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/players">Players</Nav.Link>
+          <Nav.Link href="/area">Areas</Nav.Link>
+          <Nav.Link href="/competition">Competitions</Nav.Link>
+          <Nav.Link href="/teams">Teams</Nav.Link>
+          <Nav.Link href="/games">Matches</Nav.Link>
+          <Nav.Link href="/myfavorites">My Favorites</Nav.Link>
+          {!token ? <Nav.Link href="/auth" className='login-button' >Log in/Register</Nav.Link> : <Nav.Link onClick={logOut} href="/" className='login-button' >Log out</Nav.Link>}
+        </Nav>
+      </Navbar.Collapse>
       </Container>
     </Navbar>
   </>
