35405eba38f4fcab19b5253449871a219c9f3a07
dkm94 <diane.mpacko@gmail.com>
2023-01-22 17:28:42 +0100

    new navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,46 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './Navbar.css';
 
+const navItems = [
+  {
+    title: "home",
+    path: "/"
+  },
+  {
+    title: "teams",
+    path: "/teams"
+  },{
+    title: "competitions",
+    path: "/competitions"
+  },{
+    title: "match history",
+    path: "/matchhistory"
+  },{
+    title: "news",
+    path: "/news"
+  },
+]
+
 const Navigation = ({ token }) => {
 
-  const navigate = useNavigate();
-  const logOut = () => {
-    console.log("déconnexion...")
-    localStorage.removeItem("token")
-    navigate('/');
-  }
+  // const navigate = useNavigate();
+  // const logOut = () => {
+  //   console.log("déconnexion...")
+  //   localStorage.removeItem("token")
+  //   navigate('/');
+  // }
 
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" className='nav-style' style={{ position: 'fixed', zIndex: 1000 }} bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" className='nav-style' style={{ position: 'fixed', zIndex: 1000 }}>
       <Container>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Brand href="/">2LEFOOT</Navbar.Brand>
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/players">Players</Nav.Link>
-          <Nav.Link href="/area">Areas</Nav.Link>
-          <Nav.Link href="/competition">Competitions</Nav.Link>
-          <Nav.Link href="/teams">Teams</Nav.Link>
-          <Nav.Link href="/games">Matches</Nav.Link>
-          <Nav.Link href="/myfavorites">My Favorites</Nav.Link>
-          {!token ? <Nav.Link href="/auth" className='login-button' >Log in/Register</Nav.Link> : <Nav.Link onClick={logOut} href="/" className='login-button' >Log out</Nav.Link>}
+      <Navbar.Brand href="/"><div className='logo-style'>2LEFOOT</div></Navbar.Brand>
+      <Navbar.Collapse className='justify-content-center' id="responsive-navbar-nav">
+        <Nav className="nav-items">
+          {navItems?.map((item, i) => <Nav.Link key={i} href={item?.path}>{item?.title}</Nav.Link>)}
         </Nav>
       </Navbar.Collapse>
       </Container>
