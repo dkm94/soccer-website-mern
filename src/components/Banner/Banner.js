d57f8903c583475bf09df142bbd309aa43352a35
dkm94 <diane.mpacko@gmail.com>
2023-03-28 15:36:10 +0200

    refactor Banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,45 +1,18 @@
 import React from 'react';
-import homeImg from '../../images/home.png';
-import playersImg from '../../images/players.jpeg';
-import areaImg from '../../images/areas.jpeg';
-import competitionImg from '../../images/competitions.png';
-import teamsImg from '../../images/teams.jpeg';
-import matchImg from '../../images/matches.jpeg';
+import data from './data.json';
 import './Banner.css';
 
-const images = {
-  '/games': matchImg,
-  '/players': playersImg,
-  '/area': areaImg,
-  '/competitions': competitionImg,
-  '/teams': teamsImg,
-  '/news':
-    'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltefade31f68de89f2/62fd14904a9f0d30d00a2c30/Miguel_Timm.jpg?quality=80&format=pjpg&auto=webp&width=1000',
-  default: homeImg
-};
-
-const titles = {
-  '/games': matchImg,
-  '/players': playersImg,
-  '/area': areaImg,
-  '/competitions': 'Competitions',
-  '/teams': 'Teams',
-  '/news': 'News',
-  default: 'Title'
-};
-
 const Banner = ({ path }) => {
-  const displayImg = images[path] || images.default;
-  const title = titles[path] || titles.default;
+  const element = path && data.bannerElements[path];
 
   return (
     <div
       className="banner"
       style={{
-        backgroundImage: `url('${displayImg}')`,
-        backgroundPosition: 'center'
+        backgroundImage: `url("/images/banner${element.img}")`,
+        backgroundPosition: 'top'
       }}>
-      <span className="banner-title">{title}</span>
+      <span className="banner-title">{element.title}</span>
     </div>
   );
 };
