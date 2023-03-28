236dd9e1d4ad1b4ba805e542c46c55e118a86999
dkm94 <diane.mpacko@gmail.com>
2023-03-28 13:26:20 +0200

    refactor banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,54 +7,39 @@ import teamsImg from '../../images/teams.jpeg';
 import matchImg from '../../images/matches.jpeg';
 import './Banner.css';
 
-const Banner = ({ path }) => {
-  const displayImg = (path) => {
-    switch (path) {
-      case '/games':
-        return matchImg;
-      case '/players':
-        return playersImg;
-      case '/area':
-        return areaImg;
-      case '/competitions':
-        return competitionImg;
-      case '/teams':
-        return teamsImg;
-      case '/news':
-        return 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltefade31f68de89f2/62fd14904a9f0d30d00a2c30/Miguel_Timm.jpg?quality=80&format=pjpg&auto=webp&width=1000';
-      default:
-        return homeImg;
-    }
-  };
+const images = {
+  '/games': matchImg,
+  '/players': playersImg,
+  '/area': areaImg,
+  '/competitions': competitionImg,
+  '/teams': teamsImg,
+  '/news':
+    'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltefade31f68de89f2/62fd14904a9f0d30d00a2c30/Miguel_Timm.jpg?quality=80&format=pjpg&auto=webp&width=1000',
+  default: homeImg
+};
 
-  const getTitle = (path) => {
-    switch (path) {
-      case '/games':
-        return matchImg;
-      case '/players':
-        return playersImg;
-      case '/area':
-        return areaImg;
-      case '/competitions':
-        return 'Competitions';
-      case '/teams':
-        return 'Teams';
-      case '/news':
-        return 'News';
-      default:
-        return 'Title';
-    }
-  };
+const titles = {
+  '/games': matchImg,
+  '/players': playersImg,
+  '/area': areaImg,
+  '/competitions': 'Competitions',
+  '/teams': 'Teams',
+  '/news': 'News',
+  default: 'Title'
+};
+
+const Banner = ({ path }) => {
+  const displayImg = images[path] || images.default;
+  const title = titles[path] || titles.default;
 
   return (
     <div
       className="banner"
       style={{
-        backgroundImage: `url('${displayImg(path)}')`,
+        backgroundImage: `url('${displayImg}')`,
         backgroundPosition: 'center'
-      }}
-    >
-      <span className="banner-title">{getTitle(path)}</span>
+      }}>
+      <span className="banner-title">{title}</span>
     </div>
   );
 };
