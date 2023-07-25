3542fd3c33d41b46e9e272343a90baff7ff6ba77
dkm94 <diane.mpacko@gmail.com>
2023-07-25 19:01:32 +0200

    + updated getFormattedDate tests

--- src/utils/getFormattedDate/getFormattedDate.test.js
+++ src/utils/getFormattedDate/getFormattedDate.test.js
@@ -5,7 +5,11 @@ describe('getFormattedDate() function tests', () => {
 		expect(() => getFormattedDate('something', 123)).toThrow('Invalid format');
 	});
 
-	it('should throw error if the second argument is not type string or empty string', () => {
-		expect(() => getFormattedDate('long', 123)).toThrow('Invalid date type');
+	it('should throw error if the second argument is not type string', () => {
+		expect(() => getFormattedDate('long', undefined)).toThrow('Invalid date type');
+	});
+
+	it('should throw error if the second argument is an empty string', () => {
+		expect(() => getFormattedDate('long', '')).toThrow('Invalid date type');
 	});
 });
