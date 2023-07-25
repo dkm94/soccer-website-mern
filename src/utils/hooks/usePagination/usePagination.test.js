1d4014227013fee39ee34eff7bc7fceeaa5377b6
dkm94 <diane.mpacko@gmail.com>
2023-07-25 19:42:44 +0200

    + usePagination tests

---
+++ src/utils/hooks/usePagination/usePagination.test.js
@@ -0,0 +1,15 @@
+import usePagination from 'utils/hooks/usePagination/usePagination';
+
+describe('usePagination() function tests', () => {
+	it('should throw an error if the first argument is not an array', () => {
+		expect(() => usePagination('something', 123, 123)).toThrow('data should be an array');
+	});
+
+	it('should throw an error if the second argument is not a number or equals 0', () => {
+		expect(() => usePagination([], 0, 123)).toThrow('itemsPerPage should be a valid number');
+	});
+
+	it('should throw an error if the third argument is not a string or an empty string', () => {
+		expect(() => usePagination([], 5, '')).toThrow('competition\'s value is invalid');
+	});
+});
